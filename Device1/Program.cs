d27181b97bc9f10b1ff6b97c89f3346539373bae
pwcasdf <pwcasdf@gmail.com>
2020-02-04 13:01:08 +0900

    Add project files.

---
+++ Device1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Device1
+{
+    class Program
+    {
+        private static DeviceClient deviceClient;
+        private readonly static string connectionString = "";
+
+        static void Main(string[] args)
+        {
+            deviceClient = DeviceClient.CreateFromConnectionString(connectionString, TransportType.Mqtt);
+        
+            Console.ReadLine();
+        }
+
+        private static async void d2cMsgAsync()
+        {
+
+        }
+    }
+}
