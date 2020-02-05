ee6484f885ebc9bc3464e58d38af7428f6ec4d37
pwcasdf <pwcasdf@gmail.com>
2020-02-05 10:08:16 +0900

    3 projs uploads d2c, 1 proj for reading blob

---
+++ BlobRead/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+
+
+namespace BlobRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "STORAGEACCOUNT_CONNECTIONSTRING";
+
+            // Setup the connection to the storage account
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+
+            // Connect to the blob storage
+            CloudBlobClient serviceClient = storageAccount.CreateCloudBlobClient();
+            // Connect to the blob container
+            CloudBlobContainer container = serviceClient.GetContainerReference($"device1");
+
+            ReadBlobHierarchical(container,DateTime.Now.ToString("yyyy/MM/dd"));
+
+            Console.ReadLine();
+        }
+        
+        private static async Task ReadBlobHierarchical(CloudBlobContainer container, string prefix)
+        {
+            CloudBlobDirectory dir;
+            CloudBlob cloudBlob;
+            CloudBlockBlob cloudBlockBlob;
+            BlobContinuationToken continuationToken = null;
+
+            string contents;
+
+            try
+            {
+                // Call the listing operation and enumerate the result segment.
+                // When the continuation token is null, the last segment has been returned and
+                // execution can exit the loop.
+                do
+                {
+                    BlobResultSegment resultSegment = await container.ListBlobsSegmentedAsync(prefix,
+                        false, BlobListingDetails.Metadata, null, continuationToken, null, null);
+                    foreach (var blobItem in resultSegment.Results)
+                    {
+                        // A hierarchical listing may return both virtual directories and blobs.
+                        if (blobItem is CloudBlobDirectory)
+                        {
+                            dir = (CloudBlobDirectory)blobItem;
+
+                            // Write out the prefix of the virtual directory.
+                            Console.WriteLine("Virtual directory prefix: {0}", dir.Prefix);
+
+                            // Call recursively with the prefix to traverse the virtual directory.
+                            await ReadBlobHierarchical(container, dir.Prefix);
+                        }
+                        else
+                        {
+                            // Write out the name of the blob.
+                            cloudBlob = (CloudBlob)blobItem;
+                            cloudBlockBlob = container.GetBlockBlobReference(cloudBlob.Name);
+                            contents = cloudBlockBlob.DownloadTextAsync().Result;
+
+                            Console.WriteLine("Blob name: {0}", cloudBlob.Name);
+                            Console.WriteLine(contents);
+                        }
+                        Console.WriteLine();
+                    }
+
+                    // Get the continuation token and loop until it is null.
+                    continuationToken = resultSegment.ContinuationToken;
+
+                } while (continuationToken != null);
+            }
+            catch (StorageException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+                throw;
+            }
+        }
+    }
+}
--- Device1/Program.cs
+++ Device1/Program.cs
@@ -10,18 +10,87 @@ namespace Device1
     class Program
     {
         private static DeviceClient deviceClient;
-        private readonly static string connectionString = "";
+        private readonly static string connectionString = "IOTHUB_DEVICE_CONNECTIONSTRING";
 
         static void Main(string[] args)
         {
             deviceClient = DeviceClient.CreateFromConnectionString(connectionString, TransportType.Mqtt);
-        
+
+            d2cMsgAsync();
+            c2dMsgAsync();
+
             Console.ReadLine();
         }
 
         private static async void d2cMsgAsync()
         {
+            double minTemperature = 20;
+            Random rand = new Random();
 
+            while (true)
+            {
+                double currentTemperature = minTemperature + rand.NextDouble() * 15;
+
+                string typeString = "BGM";
+                string toString = "HUB";
+                string unitString = "mgdl";
+
+                int valueInt = 100;
+                int value1Int = 100;
+                int value2Int = 100;
+                int value3Int = 100;
+                int value4Int = 100;
+                int value5Int = 100;
+                int value6Int = 100;
+
+                // Create JSON message
+                var telemetryDataPoint = new
+                {
+                    Type = typeString,
+                    To = toString,
+                    Value = valueInt,
+                    Unit = unitString,
+                    Time = DateTime.Now.ToString("yyyyMMddhhmm"),
+                    Value1 = value1Int,
+                    Value2 = value2Int,
+                    Value3 = value3Int,
+                    Value4 = value4Int,
+                    Value5 = value5Int,
+                    Value6 = value6Int,
+                    DeviceName = "device1",
+                    temperature = currentTemperature
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                // Add a custom application property to the message.
+                // An IoT hub can filter on these properties without access to the message body.
+
+                message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+
+                // Send the telemetry message
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > DEVICE1 Sending message: {1}", DateTime.Now.ToString("yyyyMMddhhmm"), messageString);
+
+                await Task.Delay(3000);
+            }
+        }
+
+        private static async void c2dMsgAsync()
+        {
+            Console.WriteLine("\nReceiving cloud to device messages from service");
+            while (true)
+            {
+                Message receivedMessage = await deviceClient.ReceiveAsync();
+                if (receivedMessage == null) continue;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Received message, DEVICE1: {0}",
+                Encoding.ASCII.GetString(receivedMessage.GetBytes()));
+                Console.ResetColor();
+
+                await deviceClient.CompleteAsync(receivedMessage);
+            }
         }
     }
 }
